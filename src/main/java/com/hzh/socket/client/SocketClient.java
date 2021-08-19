1316807f940fbe45038263487df6dede7507a805
hzhxlx <44828176+hzhxlx@users.noreply.github.com>
2021-08-19 16:58:35 +0800

    socket客户端修改

--- src/main/java/com/hzh/socket/client/SocketClient.java
+++ src/main/java/com/hzh/socket/client/SocketClient.java
@@ -7,8 +7,6 @@ import org.java_websocket.drafts.Draft_6455;
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -17,30 +15,13 @@ import java.net.URISyntaxException;
  * @author hzh
  * socket作为客户端，向服务端请求连接
  */
-@Component
-public class SocketClient implements InitializingBean {
+public class SocketClient {
 
     private static final Logger logger = LoggerFactory.getLogger(SocketClient.class);
 
-    public WebSocketClient client;
 
-    private static int count = 0;
-
-    public SocketClient() {
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        new Thread(() -> {
-            try {
-                createClient();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
-    public void createClient() throws InterruptedException {
+    public static void createClient() {
+        WebSocketClient client = null;
         try {
             client = new WebSocketClient(new URI("ws://127.0.0.1:8080/socket"), new Draft_6455()) {
                 @Override
@@ -51,16 +32,7 @@ public class SocketClient implements InitializingBean {
                 //接收消息
                 @Override
                 public void onMessage(String msg) {
-                    if (null != msg && !msg.trim().equals("")) {
-                        if (msg.equals("over")) {
-                            client.close();
-                        }
-                        try {
-                            //对消息做业务处理
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
+
                 }
 
                 @Override
@@ -73,7 +45,7 @@ public class SocketClient implements InitializingBean {
                 public void onError(Exception e) {
                     logger.error("发生错误，关闭");
                     //谨慎加这句  无限循环的可能
-                    new SocketClient().createClient();
+                    SocketClient.createClient();
                 }
             };
         } catch (URISyntaxException e) {
@@ -83,10 +55,6 @@ public class SocketClient implements InitializingBean {
         client.connect();
 
         while (!client.getReadyState().equals(WebSocket.READYSTATE.OPEN)) {
-            if (count > 0) {
-                Thread.sleep(3000000);
-            }
-            count++;
             logger.info("正在连接");
         }
     }
